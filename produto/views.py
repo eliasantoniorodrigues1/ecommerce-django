dc2926e45f638aa9e7a27b6bfcc3accdf07f89f0
Elias <eliasantoniorodrigues1@gmail.com>
2023-01-27 19:06:02 -0300

    Route configuration and creation of some views.

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class ListaProdutos(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('lista')
+
+
+class DetalheProduto(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('detalhe')
+
+
+class AdicionarAocarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('adicionaraocarrinho')
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('removerdocarrinho')
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('carrinho')
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('finalizar')
