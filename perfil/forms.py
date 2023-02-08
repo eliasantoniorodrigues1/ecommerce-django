4eeea035c0e7225aff34cf1632db155040b98025
Elias <eliasantoniorodrigues1@gmail.com>
2023-02-07 21:44:58 -0300

    forms profile and user creation

---
+++ perfil/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class PerfilForm(forms.ModelForm):
+    class Meta:
+        model = models.Perfil
+        fields = '__all__'
+        exclude = ('usuario', )
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Senha'
+        )
+    
+    def __init__(self, usuario=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.usuario = usuario
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password', 'email')
+
+    def clean(self, *args, **kwargs):
+        data = self.data
+        cleaned = self.cleaned_data
+
+        # usuarios logados: atualizacao
+        if self.usuario:
+            print('LOGADO!!!!!!!!!')
+        # usuarios nao logados: cadastro
+        else:
+            pass
+            
